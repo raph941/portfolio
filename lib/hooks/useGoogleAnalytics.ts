6676f9ddf041036c3b66bab15f4a64563628380e
raph941 <raph941@gmail.com>
2023-02-19 17:14:03 +0100

    chore: removes redundant log

--- lib/hooks/useGoogleAnalytics.ts
+++ lib/hooks/useGoogleAnalytics.ts
@@ -5,7 +5,6 @@ import { UaEventOptions } from 'react-ga4/types/ga4'
 const useGoogleAnalytics = () => {
     useEffect(() => {
         ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_UA ?? '')
-        console.log(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_UA)
 
         ReactGA.send({ hitType: 'pageview', page: window?.location.href })
     }, [])
