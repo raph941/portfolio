5e01ddd2f48f2fe81a2fde47672f0ba11733e80d
raph941 <raph941@gmail.com>
2023-02-19 17:13:02 +0100

    chore: update analytic tracker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import { ApolloProvider } from '@apollo/client'
 import { AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
-import ReactGA from 'react-ga'
 import 'antd/dist/antd.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,17 +9,13 @@ import 'react-loading-skeleton/dist/skeleton.css'
 import { Footer, StyledPageContentWrapper, NavBar } from '../components'
 import { userData } from '../data/userData'
 import client from '../lib/apollo-client'
+import { useGoogleAnalytics } from '../lib/hooks/useGoogleAnalytics'
 import { useThemeMode } from '../lib/hooks/useThemeMode'
 import MainThemeProvider from '../styles/MainThemeProvider'
 
-ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_UA ?? '')
-
 const MyApp = ({ Component, pageProps }: AppProps) => {
     const { theme, toggleThemeMode, isLightMode } = useThemeMode()
-
-    useEffect(() => {
-        ReactGA.pageview(window.location.pathname + window.location.search)
-    }, [])
+    const { trackEvent } = useGoogleAnalytics()
 
     return (
         <ApolloProvider client={client}>
@@ -30,7 +24,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
                 <StyledPageContentWrapper>
                     <AnimatePresence exitBeforeEnter onExitComplete={() => window.scrollTo(0, 0)}>
-                        <Component {...pageProps} userData={userData} />
+                        <Component {...pageProps} userData={userData} trackEvent={trackEvent} />
                     </AnimatePresence>
                 </StyledPageContentWrapper>
 
